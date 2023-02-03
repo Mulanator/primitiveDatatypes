a6073feb5e00b5db31d894a7c55ee13fb1cc204a
masteryoda@gmx.de <masteryoda@gmx.de>
2023-02-03 20:38:41 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        //-128 to 127
+        byte mByte = 12;
+
+        //-32.768 to 32.767
+        short mShort = -32000; //don't use short really
+
+        //up to 2.147.483.467
+        int mInteger= 214748411;
+
+        long mLong = 1231231231L; //add L to end
+
+        //about 6 digits
+        float mFloat = 6.35f; //use f to force float
+
+        //about 15 digits
+        double mDouble = 4.32342342;
+
+        char mChar = 'c';
+
+        boolean mBool = true;
+        
+    }
+}
